6eded4a06129783f2c76d263e11dc70b0f9a73e4
lqianglq <randycoq@163.com>
2018-10-03 11:53:33 +0800

    add pure directory

--- pure/pure-5.1.py
+++ pure/pure-5.1.py
@@ -2,7 +2,7 @@
 from keras import layers
 from keras import models
 model = models.Sequential()
-model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(28, 28, 1)))
+model.add(layers.Conv2D(32, (5, 5), activation='relu', input_shape=(28, 28, 1)))
 model.add(layers.MaxPooling2D((2, 2)))
 model.add(layers.Conv2D(64, (3, 3), activation='relu'))
 model.add(layers.MaxPooling2D((2, 2)))
@@ -11,6 +11,7 @@ model.add(layers.Flatten())
 model.add(layers.Dense(64, activation='relu'))
 model.add(layers.Dense(10, activation='softmax'))
 model.summary()
+
 #开始培训网络；
 from keras.datasets import mnist
 from keras.utils import to_categorical
